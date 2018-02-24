113cd0b9c048a87edc810b6531e481cf1fce7201
ramza007 <ramzalvo007@gmail.com>
2018-02-24 16:22:53 +0300

    made the business logic part and modified user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,25 @@
 //user interface
 
-$(document).ready(function () {
-    $("form#inputForm").submit(function (event) {
-        event.preventDefault();
-        var numbers = parsInt($("input#numbers").val());
-        $(".output").empty();
-    });
-});
+$("form#inputForm").submit(function (event) {
+    var numbers = parseInt($("input").val());
+    $(".output").empty();
 
 //end of user interface
+
+//business logic
+
+    for (var index = 1; index <= numbers; index += 1) {
+        if (index % 15 == 0) {
+            $(".output").append("<li>" + "PingPong" + "</li>");
+        } else if (index % 3 == 0) {
+            $(".output").append("<li>" + "Ping" + "</li>");
+        } else if (index % 5 == 0) {
+            $(".output").append("<li>" + "Pong" + "</li>");
+        } else {
+            $(".output").append("<li>" + index + "</li>");
+        }
+    }
+    event.preventDefault();
+});
+
+//end of business logic :-)
